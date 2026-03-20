f6a09632c274d284978f3d94252a650e416e9148
Muskatik <victoria133ct@gmail.com>
2026-03-20 18:36:46 +0200

    fixed player's death

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -1,27 +1,54 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Health))]
 public class PlayerDeath : MonoBehaviour
 {
+    public float deathAnimationDuration = 1f;
     public Transform respawnPoint;
 
+
     private Health hp;
+    private Animator animator;
+    private PlayerMovement movement;
+    private PlayerInputHandler inputHandler;
 
     private void Awake()
     {
         hp = GetComponent<Health>();
+        animator = GetComponent<Animator>();
+        movement = GetComponent<PlayerMovement>();
+        inputHandler = GetComponent<PlayerInputHandler>();
     }
 
     private void OnEnable()
     {
-        hp.OnDied += Respawn;
+        hp.OnDied += onDied;
     }
 
     private void OnDisable()
     {
-        hp.OnDied -= Respawn;
+        hp.OnDied -= onDied;
     }
+    void onDied()
+    {
+        StartCoroutine(DeathRoutine());
+    }
+    private IEnumerator DeathRoutine()
+    {
+        if (inputHandler != null) inputHandler.enabled = false;
+        if (movement != null) movement.enabled = false;
 
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null) rb.linearVelocity = Vector2.zero;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+        if (animator != null) animator.SetTrigger("Die");
+
+        yield return new WaitForSeconds(deathAnimationDuration);
+
+        Respawn();
+    }
     void Respawn()
     {
 
@@ -32,8 +59,12 @@ public class PlayerDeath : MonoBehaviour
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if (rb != null)
-            rb.linearVelocity = Vector2.zero;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.linearVelocity = Vector2.zero;
 
         hp.Heal(hp.maxHp);
+
+        if (inputHandler != null) inputHandler.enabled = true;
+        if (movement != null) movement.enabled = true;
     }
 }
